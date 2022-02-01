aedeaeab451e8f74f8dc8fbd080cb2ace4af7540
Reggie Alleyne <reggiealleyne89@gmail.com>
2022-02-01 14:01:56 -0800

    add logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faMusic} from '@fortawesome/free-solid-svg-icons'
-
+import Logo from "../images/logo.png"
 const Nav = ({libraryStatus, setLibraryStatus}) => {
     return(
         <nav>
-            <h1>Chill Vibes</h1>
+            <img src={Logo} style={{width: 60}} alt="" />
             <button onClick={() => setLibraryStatus(!libraryStatus)}>
                 Library
                 <FontAwesomeIcon icon={faMusic} style={{marginLeft: 5}} />
